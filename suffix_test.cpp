8c655bf68430ab28fcd73eaa54871b20f2926e28
Will Hawkins <whh8b@virginia.edu>
2016-06-24 00:04:34 -0400

    Add test usage of PrintSubstrings()

--- suffix_test.cpp
+++ suffix_test.cpp
@@ -1,7 +1,16 @@
 #include "suffix.hpp"
 
 #include <vector>
+#include <iostream>
 using namespace std;
+
+
+ostream &operator<<(ostream &os, const std::vector<string> &v)
+{
+	for (auto s : v)
+		os << s;
+	return os;
+}
 
 int main() {
 	std::vector<string> list_of_strings;
@@ -12,14 +21,16 @@ int main() {
 	list_of_strings.push_back("a");
 	list_of_strings.push_back("n");
 	list_of_strings.push_back("a");
-	list_of_strings.push_back("z");
 
-	SuffixTree<string> st(false);
+	SuffixTree<string> st(true);
 	SuffixTree<std::vector<string>> st_list(false);
 	st.Build("bananaz");
 	st_list.Build(list_of_strings);
 
 	cout << st << endl;
 	cout << st_list << endl;
+
+	st.PrintSubstrings(cout);
+	st_list.PrintSubstrings(cout);
 	return 1;
 }
