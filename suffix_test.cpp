77b1389d2177e4f6f2a0968d8981d8e3a6b250d4
Will Hawkins <whh8b@virginia.edu>
2016-07-05 22:40:33 -0400

    Update suffix_test to test for pointers to objects.

--- suffix_test.cpp
+++ suffix_test.cpp
@@ -4,33 +4,26 @@
 #include <iostream>
 using namespace std;
 
-
-ostream &operator<<(ostream &os, const std::vector<string> &v)
+ostream &operator<<(ostream &os, const std::vector<string*> &v)
 {
 	for (auto s : v)
-		os << s;
+		os << *s;
 	return os;
 }
 
 int main() {
-	std::vector<string> list_of_strings;
+	std::vector<string*> list_of_strings;
 
-	list_of_strings.push_back("b");
-	list_of_strings.push_back("a");
-	list_of_strings.push_back("n");
-	list_of_strings.push_back("a");
-	list_of_strings.push_back("n");
-	list_of_strings.push_back("a");
-
-	SuffixTree<string> st(true);
-	SuffixTree<std::vector<string>> st_list(false);
-	st.Build("bananaz");
+	list_of_strings.push_back(new string("b"));
+	list_of_strings.push_back(new string("a"));
+	list_of_strings.push_back(new string("n"));
+	list_of_strings.push_back(new string("a"));
+	list_of_strings.push_back(new string("n"));
+	list_of_strings.push_back(new string("a"));
+	SuffixTree<std::vector, string*> st_list(true);
 	st_list.Build(list_of_strings);
-
-	cout << st << endl;
+	cout << list_of_strings << " substrings: " << endl;
+	st_list.PrintSubstrings(cout, 0);
 	cout << st_list << endl;
-
-	st.PrintSubstrings(cout);
-	st_list.PrintSubstrings(cout);
 	return 1;
 }
